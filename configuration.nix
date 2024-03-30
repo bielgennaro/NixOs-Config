4c3c83cd6e1afac724a37c32ce77fd79af68099e
Gabriel Zirondi Di Gennaro <102267952+bielgennaro@users.noreply.github.com>
2024-03-30 02:00:37 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -10,17 +10,18 @@
       ./hardware-configuration.nix
     ];
 
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "matrix"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+	nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -39,7 +40,7 @@
     LC_NAME = "pt_BR.UTF-8";
     LC_NUMERIC = "pt_BR.UTF-8";
     LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_dTELEPHONE = "pt_BR.UTF-8";
     LC_TIME = "pt_BR.UTF-8";
   };
 
@@ -72,7 +73,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -83,57 +84,99 @@
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.gabriel = {
+  users.users.biel = {
     isNormalUser = true;
-    description = "Gabriel";
+    description = "Gabriel Zirondi Di Gennaro";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      kitty
       firefox
       thunderbird
-      spotify
-      git
-      jetbrains-toolbox
-      telegram-desktop
-      nodejs
-      ts
-      gedit
-      jetbrains-mono
-      kitty-themes
-      yarn
-      pixcat
-      neofetch
-      libreoffice
-      htop
-      alsa-tools
-          evolution
-    lutris
-    nwg-launchers
+	git
+	spotify
+	telegram-desktop
+	pixcat
+	neofetch
+	libreoffice
+  popsicle
+	alsa-tools
+	vscode
+	discord
+	gitkraken
+	jetbrains.idea-ultimate
+  jetbrains.rider
+  cura
+      gimp
+      google-chrome
+      insomnia
+      httpie
+      logseq
+      networkmanagerapplet
+      networkmanager-openvpn
 
-    # Development binaries
-    rustup
-    vscodium
-    gcc
-    binutils
-    grpc-tools
-    cargo
-    gitkraken
-      rustc
-      rustup
-      python3
-      go
-      dotnet-sdk_8
-      vscode
-      discord
-      anydesk
-      dotnet-runtime
-    nano # Used by git
+  # Gnu Utilities
+  cmake
+      gcc
+      gnumake
+      libtool
 
-    # Desktop config
-    papirus-icon-theme
+	# Dev binaries
+	rustup
+	gcc
+	binutils
+  protobuf
+	grpc-tools
+	cargo
+	rustc
+	python3
+  nodejs_21
+  yarn
+	go
+  corepack_21
+	ruby
+	dotnet-sdk_8
+	dotnet-runtime
+
+  # Terminal
+  btop
+      killall
+      pciutils
+      usbutils
+      xdg-utils
+
+  # Audio
+  pavucontrol
+      vlc
+
+  # Files
+  fd
+      gnome.file-roller
+      insync
+      pcmanfm
+      ripgrep
+      unrar
+      unzip
+      xplr
+      wget
+      zip
+
+  # Dev libraries
+  libffi
+  libxml2
+  libxslt
+  libyaml
+  libpng
+  libjpeg
+  libtiff
+  libwebp
+  libvpx
+  libvorbis
+  libogg
+
+  # Misc
+  papirus-icon-theme
+  jetbrains-mono
     ];
   };
-
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -141,17 +184,13 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-   wget
-   python3Packages.pip
-      python3Packages.virtualenv
-      python3Packages.virtualenvwrapper
-      python3Packages.django
-      python3Packages.django-cors-headers
-      python3Packages.djangorestframework
-   gnome-menus
-   gnome.gnome-tweaks
-   gnome.mutter43
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget
+	python3Packages.pip
+	python3Packages.django
+	gnome.gnome-tweaks
+	gnome-menus
+	gnome.mutter43
    gnome-browser-connector
    gnomeExtensions.vitals
    gnomeExtensions.dash-to-dock
@@ -166,23 +205,22 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  programs.mtr.enable = true;
-   programs.gnupg.agent = {
-     enable = true;
-     enableSSHSupport = true;
-   };
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
